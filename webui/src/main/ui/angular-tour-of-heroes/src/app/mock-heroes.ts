71ecc1feb06ab2bcbdf584dcef373ff125ee8a45
Andrei F <fas@live.ru>
2020-01-16 15:21:51 +0300

    Finished second lesson - https://angular.io/tutorial/toh-pt2

--- webui/src/main/ui/angular-tour-of-heroes/src/app/mock-heroes.ts
+++ webui/src/main/ui/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,6 +1,6 @@
 import { Hero } from './hero'
 
-export const HEROIS: Hero[] = [
+export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
