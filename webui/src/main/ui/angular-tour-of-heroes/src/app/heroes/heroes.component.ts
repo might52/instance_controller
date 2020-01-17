c6ac73c2dfc7d40c5ef1127623626468b4b4ef8f
Andrei F <fas@live.ru>
2020-01-17 13:18:28 +0300

    added observable and injected service

--- webui/src/main/ui/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ webui/src/main/ui/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +10,17 @@ import { HEROES } from "../mock-heroes";
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero [];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
