06e6c8f9c7945606c93760c18b1a43f5596b7503
Andrei F <fas@live.ru>
2020-01-17 16:38:14 +0300

    Fourth lesson done - https://angular.io/tutorial/toh-pt4

--- webui/src/main/ui/angular-tour-of-heroes/src/app/hero.service.ts
+++ webui/src/main/ui/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero'
 import { HEROES } from "./mock-heroes";
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,11 @@ import { HEROES } from "./mock-heroes";
 
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
