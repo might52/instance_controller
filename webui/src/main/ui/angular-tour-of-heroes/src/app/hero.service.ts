61c7076b84c7626c84b44f4e2f1367cb954fdb6a
Andrei F <fas@live.ru>
2020-01-20 17:21:34 +0300

    Final version - https://angular.io/tutorial/toh-pt5

--- webui/src/main/ui/angular-tour-of-heroes/src/app/hero.service.ts
+++ webui/src/main/ui/angular-tour-of-heroes/src/app/hero.service.ts
@@ -83,4 +83,15 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  searchHero(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
 }
