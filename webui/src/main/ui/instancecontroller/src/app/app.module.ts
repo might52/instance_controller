d947db2afed5f1305343a0dc8c5ccec187ddce15
Andrei F <fas@live.ru>
2020-02-05 20:50:39 +0300

    first layout configuration in angular

--- webui/src/main/ui/instancecontroller/src/app/app.module.ts
+++ webui/src/main/ui/instancecontroller/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
